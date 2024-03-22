0457662da8d9056ba38235a3b199bcf800a76bbc
Samia-irfan00369@gmail.com <samiairfan00369@gmail.com>
2024-03-23 03:02:45 +0500

    created number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "userguessednumber",
+        type: "number",
+        message: "please guess a number from 1-10",
+    }
+]);
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+if (answers.userguessednumber == randomNumber) {
+    console.log("Congratulations!! you've guessed the right number.");
+}
+else {
+    console.log("The number you guessed is wrong.");
+}
+;
